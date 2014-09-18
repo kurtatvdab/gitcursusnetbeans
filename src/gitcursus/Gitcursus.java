e303355c17450668afb421ed2c82e868b9b0c8f0
kurt <vannieuwenhuysenkurt@gmail.com>
2014-09-18 11:17:37 +0200

    hallo iedereen

--- src/gitcursus/Gitcursus.java
+++ src/gitcursus/Gitcursus.java
@@ -17,7 +17,7 @@ public class Gitcursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("bonjour");
+        System.out.println("hallo iedereen");
     }
     
 }
